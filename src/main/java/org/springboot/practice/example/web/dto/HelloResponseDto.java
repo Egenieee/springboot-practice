d2b65285ec056290b1f664f1631cb887aca57191
Egenieee <hhgg0925@naver.com>
2022-08-01 16:28:56 +0900

    Test code 작성 - Lombok 이용해보기

---
+++ src/main/java/org/springboot/practice/example/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package org.springboot.practice.example.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter // 선언된 모든 필드의 get 메소드를 생성해준다.
+@RequiredArgsConstructor // 선언된 모든 final 필드가 포함된 생성자를 생성해준다.
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
