273e2ee3ea9fd2244128124dbdc7ee8aaee80fe3
Egenieee <hhgg0925@naver.com>
2022-08-05 17:05:58 +0900

    Test code 오류 해결 - PostsService 에서 requestDto 받아서 제목, 내용 바꾸는 코드를 잘못 작성함

--- src/main/java/org/springboot/practice/example/service/posts/PostsService.java
+++ src/main/java/org/springboot/practice/example/service/posts/PostsService.java
@@ -25,7 +25,7 @@ public class PostsService {
         Posts posts = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
-        posts.update(posts.getTitle(), posts.getContent());
+        posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
     }
