b483de456b1ddd493d917431d75d082212b43baa
Egenieee <hhgg0925@naver.com>
2022-08-06 22:59:59 +0900

    Test code - IndexControllerTest 작성

---
+++ src/test/java/org/springboot/practice/example/web/IndexControllerTest.java
@@ -0,0 +1,30 @@
+package org.springboot.practice.example.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+    }
+}
+
+// 이 테스트는 실제로 URL 호출 시 페이지의 내용이 제대로 호출되는 지에 대한 테스트이다.
