9a0cf0c34f893380b764be5f0ae9173dfb8d797b
Egenieee <hhgg0925@naver.com>
2022-08-06 22:59:23 +0900

    feat. 기본적인 화면 생성 기능 추가 - IndexController 생성

---
+++ src/main/java/org/springboot/practice/example/web/IndexController.java
@@ -0,0 +1,17 @@
+package org.springboot.practice.example.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
+
+// 머스테치에 URL을 매핑해보자
+// 머스테치 스타터 덕분에 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정된다.
+// 앞의 경로는 src/main/resources/templates으로, 뒤의 파일 확장자는 .mustache가 붙는다.
