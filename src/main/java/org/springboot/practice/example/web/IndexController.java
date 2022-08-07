e354d3d45b0fcee5405b5b4ddc97355af0bedea7
Egenieee <hhgg0925@naver.com>
2022-08-08 01:39:26 +0900

    feat. 게시글 수정 기능 추가 - index.mustache 타이틀을 클릭하면 해당 게시글의 수정 화면으로 이동할 수 있는 a tag 추가 - index.js update fucntion 추가 - IndexController postsUpdate 메서드 추가해서 업데이트 매핑할 수 있도록 함 - posts-update.mustache 게시글 수정 화면 생성

--- src/main/java/org/springboot/practice/example/web/IndexController.java
+++ src/main/java/org/springboot/practice/example/web/IndexController.java
@@ -2,9 +2,11 @@ package org.springboot.practice.example.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springboot.practice.example.service.posts.PostsService;
+import org.springboot.practice.example.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,6 +23,15 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+
+        PostsResponseDto responseDto = postsService.findById(id);
+        model.addAttribute("post", responseDto);
+
+        return "posts-update";
     }
 }
 
