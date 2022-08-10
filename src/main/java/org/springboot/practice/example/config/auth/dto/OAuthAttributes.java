4b968f38b4f9e8663846b93bb0a574ec0e69db41
Egenieee <hhgg0925@naver.com>
2022-08-10 16:16:09 +0900

    오타 수정

--- src/main/java/org/springboot/practice/example/config/auth/dto/OAuthAttributes.java
+++ src/main/java/org/springboot/practice/example/config/auth/dto/OAuthAttributes.java
@@ -41,7 +41,7 @@ public class OAuthAttributes {
 
     // User 엔티티를 생성한다.
     // OAuthAttributes에서 엔티티를 생성하는 시점은 처음 가입할 때 이다.
-    // 가입할 때의 기본 권한을 GUEST로 주지 위해서 role 빌더 값에는 Role.GUEST를 사용한다.
+    // 가입할 때의 기본 권한을 GUEST로 주기 위해서 role 빌더 값에는 Role.GUEST를 사용한다.
     // OAuthAttributes 클래스 생성이 끝났으면 같은 패키지에 SessionUser 클래스를 생성한다.
     public User toEntity() {
         return User.builder()
