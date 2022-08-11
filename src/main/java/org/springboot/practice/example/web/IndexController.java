e8e8365a6a4028e9338cf68143814ff09fec7a2a
Egenieee <hhgg0925@naver.com>
2022-08-11 15:53:46 +0900

    refactor. IndexController LoginUser 어노테이션 사용하도록 코드 수정

--- src/main/java/org/springboot/practice/example/web/IndexController.java
+++ src/main/java/org/springboot/practice/example/web/IndexController.java
@@ -1,13 +1,15 @@
 package org.springboot.practice.example.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springboot.practice.example.config.auth.LoginUser;
 import org.springboot.practice.example.config.auth.dto.SessionUser;
 import org.springboot.practice.example.service.posts.PostsService;
 import org.springboot.practice.example.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
 import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
@@ -18,11 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) { // 기존 httpSession.getAttribute("user")로 가져오던 세션 정보를 어노테이션 하나로 가져오도록 함. 이제는 어느 컨트롤러든지 @LoginUser만 사용하면 세션 정보를 가져올 수 있다.
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user"); // CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성했다.
-        // 로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져올 수 있다.
 
         if (user != null) { // 세션에 저장된 값이 있을 때만 model에 userName으로 등록한다. 세션에 저장된 값이 없으면 model엔 아무런 값이 없는 상태이니 로그인 버튼이 보이게 된다.
             model.addAttribute("userName", user.getName());
